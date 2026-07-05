3770a56f4ebba1bbc308cb1cd9ffc68056eee5ee
Tarun Sharma <tarundadhich05@gmail.com>
2026-07-05 22:50:29 +0530

    Fix(Fundamentals): Changed data model name from FakeData to Product and created a separate composable for Cards as EachCard

--- app/src/main/java/com/tarun/androidlab/demos/fundamentals/lists/LazyVerticalGridDemo.kt
+++ app/src/main/java/com/tarun/androidlab/demos/fundamentals/lists/LazyVerticalGridDemo.kt
@@ -1,16 +1,13 @@
 package com.tarun.androidlab.demos.fundamentals.lists
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -29,11 +26,13 @@ import coil3.compose.AsyncImage
 import com.tarun.androidlab.ui.theme.AndroidLabTheme
 
 @Composable
-fun LazyVerticalGridDemo(items: List<Fakedata>) {
+fun LazyVerticalGridDemo(items: List<Product>) {
 
 
     Column(
-        Modifier.fillMaxSize().padding(10.dp),
+        Modifier
+            .fillMaxSize()
+            .padding(10.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
 
@@ -43,116 +42,151 @@ fun LazyVerticalGridDemo(items: List<Fakedata>) {
                 key = { item -> item.id }
             ) { item ->
 
+              EachCard(item = item)
 
-
-            Card(modifier = Modifier
-                .padding(8.dp)
-                .fillMaxWidth(),
-                colors = CardDefaults.cardColors(Color.Black)
-                ) {
-                Column(modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Column(Modifier.background(color = Color.Blue),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Text(text = item.text,
-                             color = Color.White,
-                            fontSize = 16.sp,
-                            fontWeight = FontWeight.ExtraBold
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    Column(modifier = Modifier.background(color = Color.Red),
-                          verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                    AsyncImage(
-                        model = item.image,
-                        contentDescription = null,
-                        Modifier.size(200.dp)
-                    )
-                    }
-
-
-                }
-            }
+//            Card(modifier = Modifier
+//                .padding(8.dp)
+//                .fillMaxWidth(),
+//                colors = CardDefaults.cardColors(Color.Black)
+//                ) {
+//                Column(modifier = Modifier.fillMaxSize(),
+//                    verticalArrangement = Arrangement.Center,
+//                    horizontalAlignment = Alignment.CenterHorizontally) {
+//                    Column(Modifier.background(color = Color.Blue),
+//                        verticalArrangement = Arrangement.Center,
+//                        horizontalAlignment = Alignment.CenterHorizontally) {
+//                        Text(text = item.text,
+//                             color = Color.White,
+//                            fontSize = 16.sp,
+//                            fontWeight = FontWeight.ExtraBold
+//                        )
+//                    }
+//                    Spacer(modifier = Modifier.height(8.dp))
+//
+//                    Column(modifier = Modifier.background(color = Color.Red),
+//                          verticalArrangement = Arrangement.Center,
+//                        horizontalAlignment = Alignment.CenterHorizontally) {
+//                    AsyncImage(
+//                        model = item.image,
+//                        contentDescription = null,
+//                        Modifier.size(200.dp)
+//                    )
+//                    }
+//
+//
+//                }
+//            }
         }
         }
     }
 
 }
 
+@Composable
+fun EachCard(item: Product, modifier: Modifier = Modifier) {
+
+    Card(modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth(),
+        colors = CardDefaults.cardColors(Color.Black)
+    ) {
+        Column(modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                Text(text = item.text,
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
+                AsyncImage(
+                    model = item.image,
+                    contentDescription = null,
+                    Modifier.size(200.dp)
+                )
+            }
 
 
-fun sampleData(): List<Fakedata>{
+        }
+    }
+}
+
+
+fun sampleData(): List<Product>{
         return listOf(
-            Fakedata(
+            Product(
                id=1,
                text = "Text1",
-               image = "https://picsum.photos/seed/picsum/200/300"
+               image = "https://picsum.photos/seed/1/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 2,
                 text = "Text 2",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/2/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 3,
                 text = "Text 3",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/3/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 4,
                 text = "Text 4",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/4/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 5,
                 text = "Text 5",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/5/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 6,
                 text = "Text 6",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/6/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 7,
                 text = "Text 7",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/7/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 8,
                 text = "Text 8",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/8/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 9,
                 text = "Text 9",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/9/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 10,
                 text = "Text 10",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/10/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 11,
                 text = "Text 11",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/11/200/300"
             ),
-            Fakedata(
+            Product(
                 id = 12,
                 text = "Text 12",
-                image = "https://picsum.photos/seed/picsum/200/300"
+                image = "https://picsum.photos/seed/12/200/300"
             )
         )
     }
 
 
 
-data class Fakedata(
+data class Product(
     val id: Int,
     val text: String,
     val image: String
