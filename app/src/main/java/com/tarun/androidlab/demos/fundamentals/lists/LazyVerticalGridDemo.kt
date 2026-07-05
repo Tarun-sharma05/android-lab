d9de4fb2a5d25a947bc94627b08e0f744eeb5808
Tarun Sharma <tarundadhich05@gmail.com>
2026-07-05 22:16:21 +0530

    Fix(Fundamentals): Fixed unecessary width

--- app/src/main/java/com/tarun/androidlab/demos/fundamentals/lists/LazyVerticalGridDemo.kt
+++ app/src/main/java/com/tarun/androidlab/demos/fundamentals/lists/LazyVerticalGridDemo.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -61,7 +62,7 @@ fun LazyVerticalGridDemo(items: List<Fakedata>) {
                             fontWeight = FontWeight.ExtraBold
                         )
                     }
-                    Spacer(modifier = Modifier.width(12.dp))
+                    Spacer(modifier = Modifier.height(8.dp))
 
                     Column(modifier = Modifier.background(color = Color.Red),
                           verticalArrangement = Arrangement.Center,
