d013ee410db263654b9517bf8f0cebc91fe6a5de
Tarun Sharma <tarundadhich05@gmail.com>
2026-07-19 00:26:35 +0530

    Feat(Networking): Completed retrofit private val for api provide with kotlin serialization. Created interface for ApiService used for called the rout in the base url.   Created object MarsApi.

--- app/src/main/java/com/tarun/androidlab/demos/networking/retrofit/MarsPhotosApp/MarsPhotosApp.kt
+++ app/src/main/java/com/tarun/androidlab/demos/networking/retrofit/MarsPhotosApp/MarsPhotosApp.kt
@@ -1,11 +1,27 @@
 package com.tarun.androidlab.demos.networking.retrofit.MarsPhotosApp
 
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
+import retrofit2.converter.kotlinx.serialization.asConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
 
-object ApiProvide {
-    fun retrofit() = Retrofit.Builder().baseUrl(BASE_URL).client(okhttp3.OkHttpClient.Builder().build())
-        .addConverterFactory()
+private val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+        .addConverterFactory(Json.asConverterFactory("Application/Json".toMediaType()))
+        .build()
+
+
+interface ApiService {
+    @GET("photos")
+    fun getPhotos(): String
+}
+
+object MarsApi{
+    val retrofitService : ApiService by lazy {
+        retrofit.create(ApiService::class.java)
+    }
 }
