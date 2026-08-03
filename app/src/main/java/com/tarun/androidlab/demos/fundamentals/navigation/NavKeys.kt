84e55932c776e8b961549e276a8ebc28b7ce6396
Tarun Sharma <tarundadhich05@gmail.com>
2026-08-03 23:53:59 +0530

    Feat (Networking): Adding MarsDetail Screen to implement Master Detail architecture.

--- app/src/main/java/com/tarun/androidlab/demos/fundamentals/navigation/NavKeys.kt
+++ app/src/main/java/com/tarun/androidlab/demos/fundamentals/navigation/NavKeys.kt
@@ -1,6 +1,15 @@
 package com.tarun.androidlab.demos.fundamentals.navigation
 
+import kotlinx.serialization.Serializable
 
 
 data object Home
 data object Product
+
+//Networking
+//Retrofit
+//MarsPhotsApp
+@Serializable
+data object MarsPhotos
+@Serializable
+data class MarsDetail(val id: String, val url: String)
---
+++ app/src/main/java/com/tarun/androidlab/demos/networking/retrofit/MarsPhotosApp/SingleMarsPhotoUI.kt
@@ -0,0 +1,9 @@
+package com.tarun.androidlab.demos.networking.retrofit.MarsPhotosApp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SingleMarsPhotoUI(modifier: Modifier = Modifier) {
+
+}
